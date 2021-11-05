c0e77504486f2a5cdbfa74235b620fe547b28bdd
서한솔 <48247286+ribeyes@users.noreply.github.com>
2021-11-06 04:46:27 +0900

    배경화면 변경. 버튼에 마우스 올렸을때 이미지 변경. 파일들 상대경로로 코드 바꿈.

--- src/djmax/IntroView.java
+++ src/djmax/IntroView.java
@@ -1,25 +1,24 @@
 package djmax;
 
+import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Graphics;
-import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
-import djmax.Music;
-
-import javax.swing.JButton;
 
 public class IntroView extends JFrame {
 	public static final int SCREEN_WIDTH = 1280;
 	public static final int SCREEN_HEIGHT = 720;
 	
 	private JPanel contentPane;
-
+	
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -41,40 +40,77 @@ public class IntroView extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		setBounds(100, 100, SCREEN_WIDTH, SCREEN_HEIGHT);
-		contentPane = new JPanel(); 
+		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		Music introMusic = new Music("introMusic.mp3", true);
-		introMusic.start();
+		//introMusic.start();
 		
 		JPanel introPanel = new JPanel() {
 			
 			public void paintComponent(Graphics g) {
-				Dimension d = getSize();
-				Image image = new ImageIcon("images/introBackground.jpg").getImage();
-				g.drawImage(image, 0, 0, d.width, d.height, null);
+				ImageIcon image = new ImageIcon(IntroView.class.getResource("../images/introBackground.jpg"));
+				g.drawImage(image.getImage(), 0, 0, null);
 			}
 		};
 		introPanel.setLayout(null);
 		introPanel.setBounds(0, 0, 1274, 691);
 		contentPane.add(introPanel);
 		
+		
 		JButton startButton = new JButton("");
-		startButton.setIcon(new ImageIcon("images/start.png"));
+		ImageIcon startButtonIcon = new ImageIcon(IntroView.class.getResource("../images/start.png"));
+		ImageIcon startButtonEnterIcon = new ImageIcon(IntroView.class.getResource("../images/startEnter.png"));
+		startButton.setIcon(startButtonIcon);
 		startButton.setBounds(95, 321, 250, 80);
 		startButton.setBorderPainted(false);
 		startButton.setContentAreaFilled(false);
 		startButton.setFocusPainted(false);
+		startButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				startButton.setIcon(startButtonEnterIcon);
+				startButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				startButton.setIcon(startButtonIcon);
+				startButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				
+			}
+		});
 		introPanel.add(startButton);
 		
+		
 		JButton exitButton = new JButton("");
-		exitButton.setIcon(new ImageIcon("images/exit.png"));
+		ImageIcon exitButtonIcon = new ImageIcon(IntroView.class.getResource("../images/exit.png"));
+		ImageIcon exitButtonEnterIcon = new ImageIcon(IntroView.class.getResource("../images/exitEnter.png"));
+		exitButton.setIcon(exitButtonIcon);
 		exitButton.setBounds(95, 422, 250, 80);
 		exitButton.setBorderPainted(false);
 		exitButton.setContentAreaFilled(false);
 		exitButton.setFocusPainted(false);
+		exitButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				exitButton.setIcon(exitButtonEnterIcon);
+				exitButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				exitButton.setIcon(exitButtonIcon);
+				exitButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				System.exit(0);
+			}
+		});
 		introPanel.add(exitButton);
 	}
 }
